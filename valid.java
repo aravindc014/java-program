02eef0cd49a78d48b7b77f7c4acb33770a96f340
Aravind c <aravindc22it@psnacet.edu.in>
2025-07-30 22:23:40 +0530

    Create valid.java

---
+++ valid.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        String time=sc.nextLine();
+        String[] t=time.split(":");
+        int h=Integer.parseInt(t[0]);
+        int m=Integer.parseInt(t[1]);
+        int s=Integer.parseInt(t[2]);
+        if(h>=0&&h<24&&m>=0&&m<60&&s>=0&&s<60){
+            System.out.println("Valid");
+        }
+        else{
+            System.out.println("Not Valid");
+        }
+    }
+}
