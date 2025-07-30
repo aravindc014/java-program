e9172b598479e5ecd1256fd9787ea0eb3938bbf7
Aravind c <aravindc22it@psnacet.edu.in>
2025-07-30 22:33:23 +0530

    corresponding.java

---
+++ corresponding.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        System.out.printf("%05d\n",n1);
+        System.out.printf("%5d\n",n2);
+        sc.close();
+    }
+}
