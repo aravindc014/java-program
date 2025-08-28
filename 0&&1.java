985d3701695c4f99b893eea7557ef376deff8317
Aravind c <aravindc22it@psnacet.edu.in>
2025-08-28 22:02:45 +0530

    Create 0&&1.java

---
+++ 0&&1.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int count=0,one=0;
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+                count++;
+            }
+            else{
+                one++;
+            }
+        }
+        System.out.println("zc = "+count);
+        System.out.println("oc = "+one);
+    }
+}
