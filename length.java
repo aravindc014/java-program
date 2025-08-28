6edef75ea121d59229f9f8c4f1145c559f589809
Aravind c <aravindc22it@psnacet.edu.in>
2025-08-28 21:43:44 +0530

    Create length.java

---
+++ length.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int count=0;
+        char[] arr =s.toCharArray();
+        for(int i : arr) count++;
+        System.out.println(count);
+    }
+}
