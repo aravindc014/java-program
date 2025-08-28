16ca88db8bd9bc201a538b8e6a2b80bf03850811
Aravind c <aravindc22it@psnacet.edu.in>
2025-08-28 23:03:12 +0530

    Create sum_even.java

---
+++ sum_even.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int sum=0;
+        for(int i=0;i<=a;i+=2){
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+}
