83c7079aa53fdfbb686da8ca28250ec9ca68541e
Aravind c <aravindc22it@psnacet.edu.in>
2025-08-28 22:13:00 +0530

    Create count_odd||even.java

---
+++ count_odd||even.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n,odd=0;
+        n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            if(arr[i]%2!=0){
+                odd++;
+            }
+        }
+        System.out.println("Odd = "+odd);
+        System.out.println("Even = "+(n-odd));
+    }
+}
