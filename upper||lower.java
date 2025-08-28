ab1f91a5165337b101f6a417524be7106269cbe8
Aravind c <aravindc22it@psnacet.edu.in>
2025-08-28 22:53:02 +0530

    Create upper||lower.java

---
+++ upper||lower.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.next().charAt(0);
+        if(ch>='A'&&ch<='Z'){
+            System.out.println("UPPERCASE");
+        }
+        else if(ch>='a'&&ch<='z'){
+            System.out.println("LOWERCASE");
+        }
+        sc.close();
+    }
+}
