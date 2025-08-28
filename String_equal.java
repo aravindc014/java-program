b734abd178354109351d06e339fc4056690e77a5
Aravind c <aravindc22it@psnacet.edu.in>
2025-08-28 21:40:21 +0530

    Create String_equal.java

---
+++ String_equal.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+    String str1 = sc.next();
+    String str2 = sc.next();
+   // boolean result = str1.equals(str2);
+
+    if(str1.equals(str2)) {
+        System.out.println("Equal");
+      }
+      else {
+        System.out.println("Not Equal" );
+      }
+    }
+}
