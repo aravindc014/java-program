d3a56505e8d844b93eadfd0fa542c8be4f6ce700
Aravind c <aravindc22it@psnacet.edu.in>
2025-08-28 22:42:55 +0530

    Create pyramid.java
       *
      ***
     *****
    *******

---
+++ pyramid.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int space=1;space<=n-i;space++){
+                System.out.print(" ");
+            }
+            for(int star=1;star<=i*2-1;star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
