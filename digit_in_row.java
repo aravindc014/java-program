4bef34d8a9963b0e8d5635727a85a9deb81bc3a5
Aravind c <aravindc22it@psnacet.edu.in>
2025-08-28 22:45:01 +0530

    Create digit_in_row.java

---
+++ digit_in_row.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextLong();
+        long count=0,ld;
+        while(n!=0){
+            ld=n%10;
+            n=n/10;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
